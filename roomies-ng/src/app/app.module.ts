d2e61f7f36b723884b13ea0a2b8d5bc69aa94a5d
Pearse Hutson <pearse.2008@gmail.com>
2018-06-15 15:03:27 -0500

    Added bill and serve proxy

--- roomies-ng/src/app/app.module.ts
+++ roomies-ng/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BillComponent } from './bill/bill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillComponent
   ],
   imports: [
     BrowserModule,
